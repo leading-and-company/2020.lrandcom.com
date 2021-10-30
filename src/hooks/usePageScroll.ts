4c0b01a72cd07df6a1b21ada55c1fb76847d5166
soichiro-nitta <soic1226@gmail.com>
2021-10-30 12:55:30 +0900

    update usePageScroll hook

--- src/hooks/usePageScroll.ts
+++ src/hooks/usePageScroll.ts
@@ -1,8 +1,10 @@
+import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 export const usePageScroll = (): void => {
+  const { asPath } = useRouter()
   useEffect(() => {
     const page = document.getElementById('page')
     if (page) page.scrollTop = 0
-  }, [])
+  }, [asPath])
 }
