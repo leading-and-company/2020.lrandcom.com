e39ac072c6e01d3854fccfb720351e71c163ed51
soichiro-nitta <soic1226@gmail.com>
2022-09-27 17:07:59 +0900

    NEXT_PUBLIC_MICROCMS_KEY

--- api/getArticle.ts
+++ api/getArticle.ts
@@ -1,9 +1,10 @@
+import { MICROCMS_KEY } from '~/lib/constants'
 import { ArticleTypes } from '~/types'
 import { request } from '~/utils/request'
 
 export default async ({ id }: { id: string }): Promise<ArticleTypes> => {
   const article = await request.get(`/articles/${id}`, {
-    headers: { 'X-MICROCMS-API-KEY': process.env.MICROCMS_KEY },
+    headers: { 'X-MICROCMS-API-KEY': MICROCMS_KEY },
   })
   return article
 }
