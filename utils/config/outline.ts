1c78b81dedde0d071ffd19f616407e2ea830e805
soichiro-nitta <soic1226@gmail.com>
2023-06-16 10:52:54 +0900

    update company address

--- utils/config/outline.ts
+++ utils/config/outline.ts
@@ -1,18 +1,18 @@
 export default [
   {
+    data: 'リーディング＆カンパニー株式会社（Leading & Company Inc.）',
     head: '会社名',
-    data: 'リーディング＆カンパニー株式会社（Leading & Company Inc.）'
   },
   {
+    data: '東京都渋谷区渋谷2-19-15 宮益坂ビルディング6階',
     head: '本社所在地',
-    data: '東京都渋谷区東1-1-37 大希青山ビル 3階'
   },
   {
+    data: '夏目 力',
     head: '代表取締役',
-    data: '夏目 力'
   },
   {
+    data: 'Webコンテンツの制作、Webサイト制作、動画制作',
     head: '事業内容',
-    data: 'Webコンテンツの制作、Webサイト制作、動画制作'
-  }
+  },
 ]
